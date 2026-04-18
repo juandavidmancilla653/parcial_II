626e1af40bd52b39157d257cf5e666ad1867fc5c
Juan David <juandavidmancilla653@gmail.com>
2026-04-17 23:52:26 -0500

    eliminar clase Guerrero

--- batalla/src/main/java/com/parcial/Main.java
+++ batalla/src/main/java/com/parcial/Main.java
@@ -1,7 +1,49 @@
 package com.parcial;
 
+import com.parcial.modelo.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // Crear criaturas
+        dragon dragon = new dragon("Draco", 100, 20);
+        mago mago = new mago("Merlin", 80, 15);
+        guerrero guerrero = new guerrero("Conan", 120, 18);
+
+        // Crear arma y equipar
+        arma espada = new arma( 10);
+        guerrero.equiparArma(espada);
+
+        // Usar habilidades
+        dragon.volar();
+        mago.lanzarHechizo();
+
+        System.out.println("\n BATALLA: Dragon vs Guerrero \n");
+
+        batalla(dragon, guerrero);
+
+        System.out.println("\n BATALLA: Mago vs Guerrero \n");
+
+        batalla(mago, guerrero);
+    }
+
+    public static void batalla(Criatura c1, Criatura c2) {
+
+        while (c1.estaViva() && c2.estaViva()) {
+
+            c1.atacar(c2);
+
+            if (c2.estaViva()) {
+                c2.atacar(c1);
+            }
+
+            System.out.println("------");
+        }
+
+        if (c1.estaViva()) {
+            System.out.println(c1.getNombre() + " ha ganado ");
+        } else {
+            System.out.println(c2.getNombre() + " ha ganado ");
+        }
     }
 }
