9a972a89e10db47ed341a8b40bdb227dd35f0a17
Juan David <juandavidmancilla653@gmail.com>
2026-04-17 23:01:28 -0500

    agregar clases Volador y magico

---
+++ batalla/src/main/java/com/parcial/Interfaces/Volador.java
@@ -0,0 +1,6 @@
+package com.parcial.Interfaces;
+
+public interface Volador {
+    void volar();
+    void aterrizar();
+}
---
+++ batalla/src/main/java/com/parcial/Interfaces/magico.java
@@ -0,0 +1,6 @@
+package com.parcial.Interfaces;
+
+public interface magico {
+    void lanzarHechizo();
+    void aprenderHechizo();
+}
