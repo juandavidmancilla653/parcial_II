0e4f58ac0230313cbfd7c05e0e9452eadb3334e4
Juan David <juandavidmancilla653@gmail.com>
2026-04-17 23:29:36 -0500

    agregar clase mago

---
+++ batalla/src/main/java/com/parcial/modelo/mago.java
@@ -0,0 +1,32 @@
+package com.parcial.modelo;
+
+import com.parcial.Interfaces.magico;
+
+public class mago extends Criatura implements magico {
+
+    public mago(String nombre, int salud, int fuerza) {
+        super(nombre, salud, fuerza);
+    }
+
+    @Override
+    public void atacar(Criatura objetivo) {
+        System.out.println(nombre + " lanza un hechizo ✨");
+        objetivo.defender(fuerza);
+    }
+
+    @Override
+    public void defender(int daño) {
+        salud -= daño;
+        System.out.println(nombre + " recibe daño. Salud: " + salud);
+    }
+
+    @Override
+    public void lanzarHechizo() {
+        System.out.println(nombre + " usa magia poderosa");
+    }
+
+    @Override
+    public void aprenderHechizo() {
+        System.out.println(nombre + " aprende un nuevo hechizo");
+    }
+}
