55173eb4d11173af59bbeb8d65459f9f29a125a3
Esteban Pantoja <pantojaesteban87@gmail.com>
2026-04-18 01:12:15 -0500

    se agrega clase dragon

---
+++ batalla/src/main/java/com/parcial/modelo/Dragon.java
@@ -0,0 +1,32 @@
+
+
+public class Dragon extends Criatura implements Volador {
+
+    public Dragon(String nombre, int salud, int fuerza) {
+        super(nombre, salud, fuerza);
+    }
+
+    @Override
+    public void atacar(Criatura objetivo) {
+        int daño = fuerza * 2;
+        System.out.println(nombre + " lanza fuego 🔥 y causa " + daño + " de daño");
+        objetivo.defender(daño);
+    }
+
+    @Override
+    public void defender(int daño) {
+        salud -= daño;
+        System.out.println(nombre + " recibe " + daño + " de daño. Salud restante: " + salud);
+    }
+
+    @Override
+    public void volar() {
+        System.out.println(nombre + " está volando 🐉");
+    }
+
+    @Override
+    public void aterrizar() {
+        System.out.println(nombre + " aterriza");
+    }
+}
+
